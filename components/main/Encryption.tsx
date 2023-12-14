dad0dfd13f12acc8a22876a33496f27169e4d49f
TsheltrimPemo <tsheltrimpemo2004@gmail.com>
2023-12-15 02:02:17 +0600

    try3

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -35,7 +35,7 @@ const Encryption = () => {
             src="/LockMain.png"
             alt="Lock Main"
             width={70}
-            height={80}
+            height={70}
             className=" z-10"
           />
         </div>
