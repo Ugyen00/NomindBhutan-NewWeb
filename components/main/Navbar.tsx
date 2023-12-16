6a16d46fae93c21fc8047c767fe3803f08d7dd7b
Sanam_App <ugyendechen00@gmail.com>
2023-12-17 00:27:18 +0600

    new nav

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,8 +4,8 @@ import React, { useState } from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+    <div className="w-full h-[50px] fixed top-2 z-50 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[25px] backdrop-blur-lg backdrop-filter bg-[#0300145e] bg-opacity-70 rounded-full border border-[#7042f861]">
         <Link href="#home" className="h-auto w-auto flex flex-row items-center">
           <Image
             src="/NavLogo.png"
@@ -17,7 +17,7 @@ const Navbar = () => {
         </Link>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-30">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] backdrop-blur-lg backdrop-filter bg-opacity-70 mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <div className="flex items-center justify-between w-full h-auto mr-[15px] px-[20px] py-[10px] text-gray-200">
             <a href="#services" className="cursor-pointer">
               Services
             </a>
